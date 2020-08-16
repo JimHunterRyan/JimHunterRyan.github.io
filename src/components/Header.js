cd98beb9dda21a3dc8bbba258fc9391ee25dda7a
J1M-RYAN <jimmih15@gmail.com>
2020-08-16 19:07:06 +0100

    added styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid black;
+`;
+const Initials = styled.h1`
+  text-align: left;
+  color: green;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+`;
+const Menu = styled.h1`
+  color: red;
+`;
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Wrapper>
+      <Initials>JR</Initials>
+      <Title>Jim Ryan | Portfolio</Title>
+      <Menu>&#9776;</Menu>
+    </Wrapper>
+  );
 };
 
 export default Header;
