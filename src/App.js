bdbde16e301ff35895af04dcd312972deeb07138
J1M-RYAN <jimmih15@gmail.com>
2020-08-16 18:48:57 +0100

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import Projects from "./components/Projects"
+import Display from "./components/Display"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Display />
+      <Projects />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
